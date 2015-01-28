551d9d36e8d36788e098ae998506c73f71eb184a
areski <areski@gmail.com>
2015-01-28 15:22:06 +0100

    update on tests

--- fs_pusher_test.go
+++ fs_pusher_test.go
@@ -10,13 +10,19 @@ func TestLoadconfig(t *testing.T) {
 	if res != true {
 		t.Error("Expected true, got ", res)
 	}
-}
 
-func TestRunCommand(t *testing.T) {
-	var res bool
-	command := []string{"touch", "file.txt"}
-	res = RunCommand(command)
-	if res != true {
-		t.Error("Expected true, got ", res)
+	var err error
+	err = ValidateConfig(config)
+	if err != nil {
+		t.Error("ValidateConfig failed ", err.Error())
 	}
 }
+
+// func TestRunCommand(t *testing.T) {
+// 	var res bool
+// 	command := []string{"touch", "file.txt"}
+// 	res = RunCommand(command)
+// 	if res != true {
+// 		t.Error("Expected true, got ", res)
+// 	}
+// }
