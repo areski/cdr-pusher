6286f653cb4512b8581eac59069621e73a33c6a3
areski <areski@gmail.com>
2015-02-03 17:37:16 +0100

    fix test TestFetch

--- fetcher_sqlite_test.go
+++ fetcher_sqlite_test.go
@@ -7,7 +7,7 @@ import (
 func TestFetch(t *testing.T) {
 	LoadConfig(defaultConf)
 	f := new(SQLFetcher)
-	f.Init(config.DBFile, config.DBTable, config.MaxPushBatch, config.CDRFields)
+	f.Init(config.DBFile, config.DBTable, config.MaxPushBatch, config.CDRFields, config.DBFlagField)
 	err := f.Fetch()
 	if err != nil {
 		t.Error("Not expected error, got ", err.Error())
