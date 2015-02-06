3b1124bdb4c1d38e79f641679e533bd20ae86e2f
areski <areski@gmail.com>
2015-02-06 01:56:12 +0100

    fix tests on TestLoadconfig

--- configfile_test.go
+++ configfile_test.go
@@ -5,13 +5,12 @@ import (
 )
 
 func TestLoadconfig(t *testing.T) {
-	var res bool
-	res = LoadConfig(defaultConf)
-	if res != true {
-		t.Error("Expected true, got ", res)
+	var err error
+	err = LoadConfig(defaultConf)
+	if err != nil {
+		t.Error("Expected nil, got ", err)
 	}
 
-	var err error
 	err = ValidateConfig(config)
 	if err != nil {
 		t.Error("ValidateConfig failed ", err.Error())
